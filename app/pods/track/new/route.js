bb504974685c14d9d24d0b831554110b4c6364f0
Andrey Listochkin <listochkin@outlook.com>
2015-02-22 14:22:54 +0200

    the simplest dialog component

--- app/pods/track/new/route.js
+++ app/pods/track/new/route.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    closeModal: function () {
+      // TODO: transition to parent route if link was send over
+      window.history.back();
+    }
+  }
 });
