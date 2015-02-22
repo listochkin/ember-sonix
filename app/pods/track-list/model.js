f29693d1f3a6c008bb10b935799aafde447f2548
Sergey Volkov <volkov.sergey@nixsolutions.com>
2015-02-22 12:23:37 +0200

    assigned tracks for track list

--- app/pods/track-list/model.js
+++ app/pods/track-list/model.js
@@ -1,15 +1,16 @@
 import DS from 'ember-data';
 
 let TrackList = DS.Model.extend({
-  title: DS.attr('string')
+  title: DS.attr('string'),
+  tracks: DS.hasMany('track', { async: true })
 });
 
 export default TrackList;
 
 TrackList.reopenClass({
   FIXTURES: [
-    { id: '11', title: 'Funky' },
-    { id: '12', title: 'Lounge' },
-    { id: '13', title: 'Hasidic Reggae Rock' }
+    { id: '11', title: 'Funky', tracks: ['21', '22', '23', '24', '25'] },
+    { id: '12', title: 'Lounge', tracks: ['26', '27', '28'] },
+    { id: '13', title: 'Hasidic Reggae Rock', tracks: ['29'] }
   ]
 });
--- app/pods/track/model.js
+++ app/pods/track/model.js
@@ -2,21 +2,22 @@ import DS from 'ember-data';
 
 let Track = DS.Model.extend({
   artist: DS.attr('string'),
-  title: DS.attr('string')
+  title: DS.attr('string'),
+  trackList: DS.belongsTo('track-list')
 });
 
 export default Track;
 
 Track.reopenClass({
   FIXTURES: [
-    { id: '21', artist: 'Daft Punk', title: 'Lose Yourself to Dance' },
-    { id: '22', artist: 'Chic', title: 'Everybody Dance' },
-    { id: '23', artist: 'Chic', title: 'Good Times' },
-    { id: '24', artist: 'Tensnake', title: 'Selfish' },
-    { id: '25', artist: 'Kool & The Gang', title: 'Tonight' },
-    { id: '26', artist: 'Plej', title: 'You' },
-    { id: '27', artist: 'Arnold T', title: 'Bang Bang' },
-    { id: '28', artist: 'Jerome Isma-Ae', title: 'Underwater Love' },
-    { id: '29', artist: 'Matisyahu', title: 'Jerusalem' }
+    { id: '21', artist: 'Daft Punk', title: 'Lose Yourself to Dance', trackList: '11' },
+    { id: '22', artist: 'Chic', title: 'Everybody Dance', trackList: '11' },
+    { id: '23', artist: 'Chic', title: 'Good Times', trackList: '11' },
+    { id: '24', artist: 'Tensnake', title: 'Selfish', trackList: '11' },
+    { id: '25', artist: 'Kool & The Gang', title: 'Tonight', trackList: '11' },
+    { id: '26', artist: 'Plej', title: 'You', trackList: '12' },
+    { id: '27', artist: 'Arnold T', title: 'Bang Bang', trackList: '12' },
+    { id: '28', artist: 'Jerome Isma-Ae', title: 'Underwater Love', trackList: '12' },
+    { id: '29', artist: 'Matisyahu', title: 'Jerusalem', trackList: '13' }
   ]
 });
