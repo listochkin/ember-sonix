80a6c33bd594052870273c03a9ec05d377779a46
Andrey Listochkin <listochkin@outlook.com>
2015-02-22 16:37:21 +0200

    don't show new tracks until they are saved

--- app/pods/track-list/model.js
+++ app/pods/track-list/model.js
@@ -1,8 +1,11 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 let TrackList = DS.Model.extend({
   title: DS.attr('string'),
-  tracks: DS.hasMany('track')
+  tracks: DS.hasMany('track'),
+
+  persistentTracks: Ember.computed.filterBy('tracks', 'isNew', false)
 });
 
 export default TrackList;
