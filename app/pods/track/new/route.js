44a50279e8b55c1a4df2e62f281b3284685be513
Andrey Listochkin <listochkin@outlook.com>
2015-02-22 16:36:58 +0200

    save new tracks to server

--- app/pods/track/new/route.js
+++ app/pods/track/new/route.js
@@ -2,6 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
+    addTrack: function (form) {
+      let trackList = this.modelFor('track-list');
+      form.trackList = trackList;
+      this.store.createRecord('track', form).save().then(() => {
+        this.transitionTo('track-list');
+      });
+    },
+
     closeModal: function () {
       // TODO: transition to parent route if link was send over
       window.history.back();
--- server/mocks/tracks.js
+++ server/mocks/tracks.js
@@ -19,8 +19,12 @@ module.exports = function(app) {
     });
   });
 
+  var next = 10;
   tracksRouter.post('/', function(req, res) {
-    res.status(201).end();
+    var json = req.body;
+    json.track.id = '2' + next;
+    next += 1;
+    res.send(201, json);
   });
 
   tracksRouter.get('/:id', function(req, res) {
@@ -45,5 +49,6 @@ module.exports = function(app) {
     res.status(204).end();
   });
 
+  app.use(require('body-parser').json());
   app.use('/api/tracks', tracksRouter);
 };
