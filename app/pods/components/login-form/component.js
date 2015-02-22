61f8d5429c44dd952a09fbb8b640984ccbc95162
Andrey Listochkin <listochkin@outlook.com>
2015-02-22 18:32:16 +0200

    how to integrate with third-party libraries

--- app/pods/components/login-form/component.js
+++ app/pods/components/login-form/component.js
@@ -3,6 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   username: null,
 
+  prepare: function () {
+    // is called after this Ember Object is created
+  }.on('init'),
+
+  setUp: function () {
+    // `this.element` points to DOM node
+    // this view occupies.
+    // Exists only in Components
+  }.on('didInsertElement'),
+
   actions: {
     login: function () {
       this.sendAction('onLogin', this.get('username'));
